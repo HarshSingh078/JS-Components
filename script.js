61cebe5e66a3b3a78fa6eb6336f7df50814da2ba
Harsh Singh <harshsinghkumwar97819@gmail.com>
2025-12-21 17:16:36 +0530

    function

--- script.js
+++ script.js
@@ -1,69 +1,123 @@
-// document.getElementById("p1").innerHTML = "Have a good day" ;
+// // document.getElementById("p1").innerHTML = "Have a good day" ;
 
-x = 5 ;
-y = 10 ;
-z = x + y ;
+// x = 5 ;
+// y = 10 ;
+// z = x + y ;
 
-console.log(z) ;
+// console.log(z) ;
 
-let a = 10 ;
-let b = "x" ;
-let c = 50 ;
+// let a = 10 ;
+// let b = "x" ;
+// let c = 50 ;
 
-console.log(a+b+c) ;
-console.log(b+a+c) ;
-console.log(b+a+c) ;
-console.log(a+c+b)
-
-
-let firstName = "Harsh" ;
-let lastName = " Singh" ;
-
-let fullName = `The fullName is : ${firstName}${lastName}` ;
-let name = firstName + lastName
-console.log(name)
-console.log(fullName)
+// console.log(a+b+c) ;
+// console.log(b+a+c) ;
+// console.log(b+a+c) ;
+// console.log(a+c+b)
 
 
-// string methods
-let text = "Hii Good Morning" 
-let text2 = "Have a good day!"
-console.log(text.length) ;
-console.log(text.charAt(0))
-console.log(text.charCodeAt(0))
-console.log(text.concat(" ", text2))
-console.log(text.slice(5))
-console.log(text.slice(5,15)) 
-console.log(text.slice(-15)) 
+// let firstName = "Harsh" ;
+// let lastName = " Singh" ;
 
-console.log(text.toLocaleLowerCase())
-console.log(text.toLocaleUpperCase())
-console.log(text.trim())
-console.log(text.repeat(2))
-console.log(text.replace("Hii","Hey"))
-
-// search , match , matchAll , startsWith , endsWith , includes , indexOf , lastIndexOf 
-
-let num1 = "100" 
-let num2 = "50" 
-let prod =  num1 * num2 ;
-let prod2 =  num1 - num2 ;
-let prod3 =  num1 / num2 ;
-console.log(prod)
-console.log(prod2)
-console.log(prod3)
+// let fullName = `The fullName is : ${firstName}${lastName}` ;
+// let name = firstName + lastName
+// console.log(name)
+// console.log(fullName)
 
 
+// // string methods
+// let text = "Hii Good Morning" 
+// let text2 = "Have a good day!"
+// console.log(text.length) ;
+// console.log(text.charAt(0))
+// console.log(text.charCodeAt(0))
+// console.log(text.concat(" ", text2))
+// console.log(text.slice(5))
+// console.log(text.slice(5,15)) 
+// console.log(text.slice(-15)) 
+
+// console.log(text.toLocaleLowerCase())
+// console.log(text.toLocaleUpperCase())
+// console.log(text.trim())
+// console.log(text.repeat(2))
+// console.log(text.replace("Hii","Hey"))
+
+// // search , match , matchAll , startsWith , endsWith , includes , indexOf , lastIndexOf 
+
+// let num1 = "100" 
+// let num2 = "50" 
+// let prod =  num1 * num2 ;
+// let prod2 =  num1 - num2 ;
+// let prod3 =  num1 / num2 ;
+// console.log(prod)
+// console.log(prod2)
+// console.log(prod3)
 
 
 
-// number methods 
-let NUM1 = 100 ;
-console.log(NUM1.toString())
-console.log(typeof(NUM1))
 
-// toexponential , toPrecision() , valueOf() , 
-console.log(Number(num1))
-console.log(Number(true))
-console.log(Number(false))
+
+// // number methods 
+// let NUM1 = 100 ;
+// console.log(NUM1.toString())
+// console.log(typeof(NUM1))
+
+// // toexponential , toPrecision() , valueOf() , 
+// console.log(Number(num1))
+// console.log(Number(true))
+// console.log(Number(false))
+// console.log(parseInt(num1))
+
+// console.log(Number.isInteger(10))
+// console.log(Number.isFinite(10))
+// console.log(Number.isNaN(10))
+// console.log(Number.isSafeInteger(10))
+
+// console.log(Number.parseInt(num1))
+
+// // Bitwise 
+// // & , | , ~ , ^ , << , >> , >>>
+
+
+
+// // table of a number 
+// // let number = prompt("Enter your number") 
+// // for(let i=1;i<=10;i++) {
+// //     console.log(`The table of ${number} is : ${number*i}`) ;
+// // }
+
+
+// // function declaration 
+// function add(a ,b) {
+//     return a + b ;
+// }
+
+// let sum = add(5,10) ;
+// console.log(sum) ;
+
+// // default parameters
+// function product(x , y=10) {
+//     return x * y ;
+// }
+
+// let result = product(5) ;
+// console.log(result)
+
+
+// // function expressions
+// const x = function (a, b) {return a * b};
+
+// let z = x(4, 3);
+
+
+// function call before the function declaration
+// let sum1 = add(2,3) 
+// function add(a,b) {return  a + b }
+// console.log(sum1)
+
+
+// // arrow function 
+// let myFunction = (a , b) => {return a+b} ;
+// let result = myFunction(11 , 13)
+// console.log(result)
 
