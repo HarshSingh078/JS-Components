15bcf5193794e405ea61eaaa22435579d6e29806
Harsh Singh <harshsinghkumwar97819@gmail.com>
2025-12-21 11:09:27 +0530

    string + arithmetic

---
+++ script.js
@@ -0,0 +1,25 @@
+// document.getElementById("p1").innerHTML = "Have a good day" ;
+
+x = 5 ;
+y = 10 ;
+z = x + y ;
+
+console.log(z) ;
+
+let a = 10 ;
+let b = "x" ;
+let c = 50 ;
+
+console.log(a+b+c) ;
+console.log(b+a+c) ;
+console.log(b+a+c) ;
+console.log(a+c+b)
+
+
+let firstName = "Harsh" ;
+let lastName = " Singh" ;
+
+let fullName = `The fullName is : ${firstName}${lastName}` ;
+let name = firstName + lastName
+console.log(name)
+console.log(fullName)
